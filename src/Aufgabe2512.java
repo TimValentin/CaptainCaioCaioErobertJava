5f4f73f8fc8588d1028f53529ecb2dd657b75c39
TimHCJS <tim.valentin23@hotmail.com>
2022-09-11 17:16:13 +0200

    initial commit

---
+++ src/Aufgabe2512.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Aufgabe2512 {
+    public static void main(String[] args) {
+        int width;
+
+        int height;
+        int counter = 0;
+
+
+        Scanner scan = new Scanner(System.in);
+        width = scan.nextInt();
+        System.out.printf("aaaa");
+        System.out.println("aaaaa");
+        height = (int) (width / 2 + 0.5);
+    }
+}
