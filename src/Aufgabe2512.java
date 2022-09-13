781aa49468cf16baea62b14fa942ea0390b90df8
TimHCJS <tim.valentin23@hotmail.com>
2022-09-13 09:43:29 +0200

    Update 2512 works now

--- src/Aufgabe2512.java
+++ src/Aufgabe2512.java
@@ -5,8 +5,10 @@ public class Aufgabe2512 {
         double input;
         double yLenghtD;
         int yLenght;
-        int xLenght;
         int x = 0;
+        int xLenght;
+        int sLenght;
+        int s = 0;
         int stars = 0;
 
         Scanner scan = new Scanner(System.in);
@@ -14,18 +16,27 @@ public class Aufgabe2512 {
         input = scan.nextInt();
         yLenghtD = input / 2 + 0.5;
         yLenght = (int)yLenghtD;
+        xLenght = (int)yLenghtD;
 
 
         for (int y = 1; y < yLenght + 1; y++){
-            xLenght = y + stars;
-            for (x = 0; x < xLenght; x++)
+            while (x < xLenght){
+                System.out.print(" ");
+                x++;
+            }
+            x = 0;
+            xLenght--;
+
+            sLenght = y + stars;
+            for (s = 0; s < sLenght; s++)
                 System.out.print("*");
 
-            System.out.println();
-            x = 0;
+            if (x != xLenght)
+                System.out.println();
+
+            s = 0;
             stars++;
         }
-
 
     }
 }
